d6d1c0506f322280ffa93ad613a72ce6a32b7a9c
Shaun Sheppard <shaun@fanaticusxr.com>
2022-01-28 12:58:05 -0500

    updated scale-tween to actually tween instead of random chaos

--- src/components/custom-behavior.js
+++ src/components/custom-behavior.js
@@ -28,8 +28,28 @@ AFRAME.registerComponent("custom-behavior", {
 );
 
 AFRAME.registerComponent("scale-tween", {
+  schema: {
+    smallest: { type: "number", default: 0.5 },
+    largest: { type: "number", default: 3 },
+    oneWayTimeMillis: { type: "number", default: 2000 },
+    startTimeMillis: {type: "number", default: 0},
+    isGrowing: { type: "boolean", default: true},
+    startScale:  { type: "number", default: 0.5 },
+    targetScale:  { type: "number", default: 0.5 }
+  },
+  
   async init() {
     await waitForDOMContentLoaded();
+
+    this.toggleDirection(Date.now());
+  },
+
+  toggleDirection(newStartMillis) {
+    this.data.isGrowing = !this.data.isGrowing;
+    const { smallest, largest, isGrowing } = this.data;
+    this.data.startScale = isGrowing ? smallest : largest;
+    this.data.targetScale = isGrowing ? largest : smallest;
+    this.data.startTimeMillis = newStartMillis;
   },
 
   play() {
@@ -41,9 +61,19 @@ AFRAME.registerComponent("scale-tween", {
   },
 
   tick() {
-    const scale = Math.random() * 3;
-    this.el.object3D.scale.set(scale, scale, scale);
-    this.el.object3D.matrixNeedsUpdate = true;
+    const { startScale, targetScale, oneWayTimeMillis, startTimeMillis } = this.data;
+
+    const start = new THREE.Vector3(startScale, startScale, startScale);
+    const target = new THREE.Vector3(targetScale, targetScale, targetScale);
+    const nowMillis = Date.now()
+    const lerp = (nowMillis - startTimeMillis) / oneWayTimeMillis;
+
+    if (lerp > 1) {
+      this.toggleDirection(nowMillis);
+    } else {
+      const scale = start.lerp(target, lerp);
+      this.el.object3D.scale.set(scale.x, scale.y, scale.z);
+      this.el.object3D.matrixNeedsUpdate = true;
+    }
   }
-}
-);
+});
